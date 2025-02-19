25af75dfec80b185438472cec4156e26d4092080
techfish <msuke1018@icloud.com>
2025-02-19 18:08:38 +0900

    クエリURLと合成URLを更新して新しいサーバーアドレスを反映

--- test.py
+++ test.py
@@ -9,7 +9,7 @@ with open("text.txt", "w", encoding="utf-8") as file:
 with open("text.txt", "r", encoding="utf-8") as file:
     text_data = file.read()
 
-query_url = "http://192.168.1.12:50021/audio_query"
+query_url = "http://192.168.1.15:50021/audio_query"
 query_params = {"speaker": 1, "text": text_data}
 query_response = requests.post(query_url, params=query_params)
 
@@ -17,7 +17,7 @@ with open("query.json", "w", encoding="utf-8") as file:
     file.write(query_response.text)
 
 # 音声合成を実行
-synthesis_url = "http://192.168.1.12:50021/synthesis"
+synthesis_url = "http://192.168.1.15:50021/synthesis"
 synthesis_headers = {"Content-Type": "application/json"}
 with open("query.json", "r", encoding="utf-8") as file:
     synthesis_data = file.read()
